13d09585669f183ac93067eeb2b0980c517bdd1f
David <muzaliwadavid44@gmail.com>
2024-08-30 09:52:18 -0500

    added files

---
+++ main.py
@@ -0,0 +1,32 @@
+
+def verse1():
+    print("I change, you change (change)")
+    print("Why would one stay the same?")
+    print("That's no fun")
+    print("You're strange, so strange(strange)")
+    print("If you choose to remain the same\n")
+
+def verse2():
+    print("When I say change, I mean evolve")
+    print("All my old friends they just dissolve")
+    print("Because they stayed on the same old shit")
+    print("Just hung out off campus smokin' this")
+    print("You see me, I'm not the type of guy")
+    print("But I used to before I found my high")
+    print("But they can't grasp, they don't know why")
+    print("I'm not the same Steve they used to like\n")
+
+def verse3():
+    print("I change, you change (change)")
+    print("Why would one stay the same?")
+    print("That's no fun")
+    print("You're strange, so strange (strange)")
+    print("If you choose to remain the same")
+def verse4():
+    name=input("David Muzaliwa")
+    print("hello,"+name)
+if __name__ == "__main__":
+    verse1()
+    verse2()
+    verse3()
+    verse4()
