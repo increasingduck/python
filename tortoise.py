b7e58222aeadb273b6a9525dc7eefd2ea9b4c67d
David <muzaliwadavid44@gmail.com>
2024-09-09 10:33:17 -0500

    turtle

---
+++ tortoise.py
@@ -0,0 +1,11 @@
+from turtle import Turtle
+
+def my_function():
+    bob = Turtle()
+    bob.shape("turtle")
+    bob.color("red")
+    
+    bob.screen.mainloop()
+
+if __name__ == "__main__":
+    my_function()
