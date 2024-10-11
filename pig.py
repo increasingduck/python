679a94535601bbcd7d01db9f539b736506cafc9b
David <muzaliwadavid44@gmail.com>
2024-10-11 09:48:50 -0500

    rock paper scissors
    Co-authored-by: slentzik <slentzik@users.noreply.github.com>

--- pig.py
+++ pig.py
@@ -7,7 +7,23 @@ def Player1turn():
 def (x = "y")
   
 if x == "y":
-
+ X = random.randint(1,6)
+     
+    if no == 1:
+        print("[1]")
+    if no == 2:
+        print("[2]")
+    if no == 3:
+        print("[3]")
+    if no == 4:
+        print("[4]")
+    if no == 5:
+        print("[5]")
+    if no == 6:
+        print("[6]")
+         
+    x=input("press y to roll again and n to exit:")
+    print("\n")
 def Player2turn():
     print ("Press X to roll dice")
 
